af93da236549b9a4eb820c33cce5b31d6c45fa14
Gabriel Torres <elcomparosh97@gmail.com>
2024-02-19 20:57:11 -0600

    Agregar archivos de proyecto.

---
+++ CalculateForce/Force.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateForce
+{
+    internal class Force
+    {
+		private double mass;
+
+		public double Mass
+		{
+			get { return mass; }
+			set { mass = value; }
+		}
+
+		private double acceleration;
+
+		public double Acceleration
+        {
+			get { return acceleration; }
+			set { acceleration = value; }
+		}
+
+        public Force()
+        {
+			mass = 0;
+			acceleration = 0;
+        }
+
+		public double CalculeteForce() 
+		{
+			return mass*acceleration;
+		}
+    }
+}
---
+++ CalculateForce/Program.cs
@@ -0,0 +1,9 @@
+﻿using CalculateForce;
+
+Force blow = new Force();
+blow.Mass = 23;
+blow.Acceleration = 56;
+
+double blowForce = blow.CalculeteForce();
+Console.WriteLine("The force of the blow with mass of: "+blow.Mass+ " Kg and with acceleration of: "+blow.Acceleration+ " m/s² It is: " + blowForce+" Newtons");
+Console.ReadKey();
